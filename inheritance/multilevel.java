04ca3af13ef539fb706b1425adc824bb2bfefd83
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-12-03 17:22:19 +0545

    oops in java

---
+++ inheritance/multilevel.java
@@ -0,0 +1,14 @@
+class multilevel {
+
+    class A {
+        void msg() {
+            System.out.println("Hello");
+        }
+
+    }
+
+    public static void main(String args[]) {
+        C obj = new C();
+        obj.msg();
+    }
+}
